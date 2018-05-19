53f162f70d7b82129d13fab6cf441030be8cea57
gaoxk <kristy@crazy4pandas.gao@gmail.com>
2018-05-19 19:59:18 -0400

    contact section mobile friendly

--- magic.js
+++ magic.js
@@ -1,99 +1,67 @@
-/*// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-var navbar;
-var sticky;
-
 window.onload = function(){
-	// Get the navbar
-	navbar = document.getElementById("navbar");
 
-	// Get the offset position of the navbar
-	sticky = navbar.offsetTop;
-};
+console.log(window.innerWidth);
 
-
-
-
-
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    console.log("adding stick");
-  } else {
-    navbar.classList.remove("sticky");
-    console.log("remove stick");
-  }
-}	 */
-
-window.onload = function(){
+var $window = $(window);
+var $body = $('body');
 
 function load_screen(){
   $(".loading").fadeOut(500);
   $(".to-animate").addClass("animated fadeInUp");     
 }
 
-setTimeout(load_screen, 2000);    
+setTimeout(load_screen, 1);    
 
 //nav bar stuff
-$(window).scroll(function(){
-  if ($(window).scrollTop() < $(window).height() /2 ) {
+$window.scroll(function(){
+  if ($window.scrollTop() < $window.height() /2 ) {
     $('nav').fadeIn('fast');
   }else{
     $('nav').fadeOut('fast');
   }
 });
 
-$( "body" ).mousemove(function( event ) {
-  if (!($(window).scrollTop() < $(window).height()-50)) {
-    if (event.pageY - $(window).scrollTop() > 50) {
+$body.mousemove(function( event ) {
+  if (!($window.scrollTop() < $window.height()-50)) {
+    if (event.pageY - $window.scrollTop() > 50) {
      $('nav').fadeOut('fast');
     }else {
       $('nav').fadeIn('fast');
     }
-  }
-  
+  }  
 });
 
 // nav bar navigation
-  $('body').scrollspy({target: ".navbar", offset: 50});   
-
-  // Add smooth scrolling on all links inside the navbar
-  $("#myNavbar a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  
-  });
+$body.scrollspy({target: ".navbar", offset: 50});   
+// Add smooth scrolling on all links inside the navbar
+$("#myNavbar a").on('click', function(event) {
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash
+    var hash = this.hash;
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  }  
+});
 
 
 //fadeing in elements on scroll
-var $window = $(window);
 function isScrolledIntoView($elem, $window) {
     var docViewTop = $window.scrollTop();
     var docViewBottom = docViewTop + $window.height();
-
     var elemTop = $elem.offset().top;
     var elemBottom = elemTop + $elem.height();
-
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
+
 $(document).on("scroll", function () {
     if (isScrolledIntoView($('#letstalk'), $window)) {
       $('#letstalk').addClass("animated fadeInUp");
@@ -108,3 +76,8 @@ $(document).on("scroll", function () {
 
 
 };
+
+$(window).resize(function () {
+  console.log(window.innerWidth);
+  console.log($('#contact').innerWidth);
+});
